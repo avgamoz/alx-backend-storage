e7e428e2716da5d075da5863fb8bc40413a8630c
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2023-07-12 19:22:06 +0100

    101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,30 +1,19 @@
 -- create a stored procedure
 -- compute and store the average weighted score for all students.
+-- creates a stored procedure ComputeAverageWeightedScoreForUsers
+-- that computes and stores the average weighted score for all students
 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER |
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DROP TEMPORARY TABLE IF EXISTS temp_weights;
-    CREATE TEMPORARY TABLE temp_weights (
-        user_id int,
-        weighted_score float
-    );
-
-    -- calculate the weights
-    INSERT INTO temp_weights (user_id, weighted_score)
-    SELECT user_id, SUM(score) / COUNT(*) AS weighted_score
-    FROM corrections
-    GROUP BY user_id;
-
-    -- Update the avarage score 
-    UPDATE users
-   	SET average_score=(SELECT AVG(weighted_score) FROM temp_weights)
-	WHERE id IN (SELECT user_id FROM temp_weights);
-    
-    -- Drop temporary
-    DROP TABLE temp_weights;
-
-
-END;$$
-DELIMITER ;
+  UPDATE users AS U, 
+    (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
+    FROM users AS U 
+    JOIN corrections as C ON U.id=C.user_id 
+    JOIN projects AS P ON C.project_id=P.id 
+    GROUP BY U.id)
+  AS WA
+  SET U.average_score = WA.w_avg 
+  WHERE U.id=WA.id;
+END;
