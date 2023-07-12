905e20b91190ab12eb4241c9b40aff6a55d4868b
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2023-07-12 19:20:48 +0100

    100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,32 +1,37 @@
 -- store average 
 -- With an input with user_id
+-- This SQL script creates a stored procedure named 'ComputeAverageWeightedScoreForUser' which takes in one parameter user_id.
+-- It computes the average weighted score for a user by summing up the product of scores and weights of the user from the 'corrections' table, summing up the total weight, and then dividing the total weighted score by the total weight, and then updating the 'average_score' field of the user in the 'users' table.
 
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_score FLOAT DEFAULT 0;
-    DECLARE total_weight FLOAT DEFAULT 0;
-    DECLARE weighted_score FLOAT DEFAULT 0;
-    DECLARE num_projects INT DEFAULT 0;
-    DECLARE avg_weighted_score FLOAT DEFAULT 0;
-    
-    SELECT COUNT(*) INTO num_projects FROM projects;
-    
-    SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
-    
-    SELECT 
-        SUM(CASE project_id WHEN @project_c THEN score ELSE 0 END) as c_score,
-        SUM(CASE project_id WHEN @project_py THEN score ELSE 0 END) as py_score
-    FROM corrections
-    WHERE user_id = user_id;
-    
-    SET weighted_score = (c_score * 0.6) + (py_score * 0.4);
-    SET total_weight = num_projects * 100;
-    
-    IF total_weight > 0 THEN
-        SET avg_weighted_score = (weighted_score / total_weight) * 100;
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
+
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
     END IF;
-    
-    UPDATE users SET average_score = avg_weighted_score WHERE id = user_id;
 END $$
 DELIMITER ;
